7c6c6061f3018f9047ce615779bbd577c6c7aff7
SauravBista10 <120402314+SauravBista10@users.noreply.github.com>
2022-12-12 21:58:16 +0545

    lab 1-network commands

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+//import statement
+////first program in java///
+
+public class FirstProgram{
+
+     public static void main(String[]brgs){
+	     int a;
+		
+		System.out.println("hello developers ");
+		
+	}
+}
